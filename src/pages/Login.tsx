eb7072f2e22f746e3df599b251f927ea18f014aa
Alexander Mattos <mattosalexander@gmail.com>
2025-10-21 18:04:11 -0300

    Comment out "Create new account" section on Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -107,7 +107,7 @@ const Login: React.FC = () => {
               </div>
             </form>
 
-            <div className="mt-6">
+            {/* <div className="mt-6">
               <div className="relative">
                 <div className="absolute inset-0 flex items-center">
                   <div className="w-full border-t border-gray-300"></div>
@@ -127,7 +127,7 @@ const Login: React.FC = () => {
                   Criar nova conta
                 </Link>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
